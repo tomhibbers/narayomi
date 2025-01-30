d0b2e586c0b4b449645c2d9ced080dae0ce23cf4
Tom Hibbers <thomas.hibbers@angloamerican.com>
2025-01-30 14:28:50 +0200

    Scaffold initial pages and nav

---
+++ lib/pages/browse_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrowsePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Browse Page", style: TextStyle(fontSize: 20)),
+    );
+  }
+}
---
+++ lib/pages/updates_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UpdatesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Updates Page", style: TextStyle(fontSize: 20)),
+    );
+  }
+}
